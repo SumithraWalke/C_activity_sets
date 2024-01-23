ecd20c09445d5613859d5c52c4877e0346d5591f
SumithraWalke <sumithrar.bsc23@rvu.edu.in>
2024-01-23 17:59:26 +0530

    committed 1/23/2024, 5:59:25 PM

--- set01/problem05.c
+++ set01/problem05.c
@@ -27,5 +27,5 @@ int compare(int a, int b, int c)
 }
 void output(int a, int b, int c, int largest)
 {
-    printf("the largest of %d,%d")
+    printf("the largest of %d,%d and %d is %d",a,b,c,largest);
 }
