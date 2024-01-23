d805e044697bc1fe5a3e8d53292b8cdf49baaded
SumithraWalke <sumithrar.bsc23@rvu.edu.in>
2024-01-23 18:08:57 +0530

    committed 1/23/2024, 6:08:55 PM

--- set01/problem06.c
+++ set01/problem06.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
-int input(int *a, int *b, int *c)
+void input(int *a, int *b, int *c)
 {
     printf("Enter the numbers:\n");
     scanf("%d %d %d",a,b,c);
-    
 
 }
+void compare
