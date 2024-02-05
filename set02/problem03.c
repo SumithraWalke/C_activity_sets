802a2fd98786d45b1434351c10555f1da59b8135
SumithraWalke <sumithrar.bsc23@rvu.edu.in>
2024-02-05 14:12:49 +0530

    test

---
+++ set02/problem03.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int input_number()
+{
+    int n;
+    printf("enter the number:\n");
+    scanf("%d",&n);
+    return n;
+}
+int is_composite(int n)
+{
+    if(n<=1)
+    {
+        return 0;
+    }
+    else 
+    return 1;
+    for(int i=2;i<n/2;i++)
+    {
+        if (n%2==0)
+    {
+        return 1;
+    }
+    return 0;
+}
+}
+void output(int n,int result)
+{
+    if(result==1)
+    {
+    printf("%d is a composite number.",n);
+    }
+    else 
+    {
+        printf("%d is not a composite number.",n);
+    }
+}
+int main()
+{
+    int n,result;
+    n=input_number();
+    result=is_composite(n);
+    output(n,result);
+    return 0;
+}
