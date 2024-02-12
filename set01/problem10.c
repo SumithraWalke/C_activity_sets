5c3ecfa595d388578cdfa88279cff4d297e1fc8e
SumithraWalke <sumithrar.bsc23@rvu.edu.in>
2024-02-12 15:04:38 +0530

    committed 2/12/2024, 3:04:37 PM

--- set01/problem10.c
+++ set01/problem10.c
@@ -9,7 +9,7 @@ void input_two_strings(char *string1, char *string2)
 int stringcompare(char *string1,char *string2)
 {
     int i;
-    for(i=0;string1[i]==string2[i] && string1[i]=='\0';i++);
+    for(i=0;string1[i]==string2[i] && string1[i]!='\0';i++);
     return string1[i]-string2[i];
 }
 void output(char *string1,char *string2,int result)
