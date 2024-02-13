1c85aa4a085a0ecfa12ac8a44ccaced54e586d57
SumithraWalke <sumithrar.bsc23@rvu.edu.in>
2024-02-13 18:41:41 +0530

    committed 2/13/2024, 6:41:41 PM

--- set02/problem06.c
+++ set02/problem06.c
@@ -1,5 +1,31 @@
 #include<stdio.h>
+#include<string.h>
 void input_string(char *a)
 {
-    printf
+    printf("enter the string:");
+    scanf("%s",a);
+}
+void str_reverse(char *str, char *rev_str)
+{
+    int length=strlen(str);
+    for(int i=0; i<length; i++)
+    {
+        rev_str[i]=str[length-1-i];
+    }
+    rev_str[length]='\0';
+    
+}
+void output(char *a, char *rev_str)
+{
+    printf("the original string is %s\n",a);
+    printf("the reversed string is %s",rev_str);
+}
+int main()
+{
+    char a[100];
+    char rev_str[100];
+    input_string(a);
+    str_reverse(a,rev_str);
+    output(a,rev_str);
+    return 0;
 }
