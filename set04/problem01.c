e56447c63e6e1beb49340766d42b36da5985fb34
SumithraWalke <sumithrar.bsc23@rvu.edu.in>
2024-02-27 14:05:09 +0530

    committed 2/27/2024, 2:05:08 PM

--- set04/problem01.c
+++ set04/problem01.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+
+int gcd(int a, int b) {
+    if (b == 0) return a;
+    else return gcd(b, a % b);
+}
+
 void input(int *num1, int *den1, int *num2, int *den2) {
     printf("Enter first fraction (numerator/denominator): ");
     scanf("%d/%d", num1, den1);
@@ -6,27 +12,20 @@ void input(int *num1, int *den1, int *num2, int *den2) {
     printf("Enter second fraction (numerator/denominator): ");
     scanf("%d/%d", num2, den2);
 }
-void gcd(int a, int b) {
-    if (b == 0) return a;
-    else return gcd(b, a % b);
-}
 
-void add(int num1, int den1, int num2, int den2, int *res_num, int *res_den)
- {
+void add(int num1, int den1, int num2, int den2, int *res_num, int *res_den) {
     int g = gcd(den1, den2);
-    *res_num= (num1*den2 + num2*den1)/g;
-    *res_den= den1*den2/g;
- }
-
-
+    *res_num = (num1 * den2 + num2 * den1) / g;
+    *res_den = den1 * den2 / g;
+}
 
 void output(int num1, int den1, int num2, int den2, int res_num, int res_den) {
     printf("The sum of %d/%d and %d/%d is %d/%d\n", num1, den1, num2, den2, res_num, res_den);
 }
+
 int main() {
     int num1, den1, num2, den2, res_num, res_den;
     input(&num1, &den1, &num2, &den2);
-    void gcd(a,b);
     add(num1, den1, num2, den2, &res_num, &res_den);
     output(num1, den1, num2, den2, res_num, res_den);
     return 0;
