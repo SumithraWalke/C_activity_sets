2236ecf5d2d097ba39b4e5586a6205e618111ce5
SumithraWalke <sumithrar.bsc23@rvu.edu.in>
2024-03-01 19:25:51 +0530

    committed 3/1/2024, 7:25:51 PM

--- set04/problem03.c
+++ set04/problem03.c
@@ -1,39 +1,27 @@
 #include <stdio.h>
-
-void input_n_and_r(int *n, int *r) {
-    printf("Enter the values of n and r: ");
-    scanf("%d %d", n, r);
+void input_n_and_r(int *n, int *r){
+  printf("enter n and r: ");
+  scanf("%d %d",n,r);
 }
 
-int nCr(int n, int r) {
-    if (r > n) {
-        return -1; 
-    }
-    if (r == 0 || n == 0) {
-        return 1;
-    }
-    int numerator = 1, denominator = 1;
-    for (int i = n; i > n - r; i--) {
-        numerator *= i;
-    }
-    for (int i = r; i > 0; i--) {
-        denominator *= i;
-    }
-    return numerator / denominator;
+int fac(int n){
+  int fact=1;
+  for(int i=n;i>1;i--){
+    fact*=i;
+  }
+  return fact;
+}
+int nCr(int n, int r){
+  int f=fac(n)/(fac(r)*fac(n-r));
+  return f;
+}
+void output(int result){
+  printf("%d",result);
 }
 
-void output(int n, int r, int result) {
-    if (result == -1) {
-        printf("The value of nCr is undefined because r cannot be greater than n.\n");
-    } else {
-        printf("The value of nCr is: %d\n", result);
-    }
-}
-
-int main() {
-    int n, r, result;
-    input_n_and_r(&n, &r);
-    result = nCr(n, r);
-    output(n, r, result);
-    return 0;
+int main(){
+int n,r;
+  input_n_and_r(&n,&r);
+  int f=nCr(n,r);
+  output(f);
 }
